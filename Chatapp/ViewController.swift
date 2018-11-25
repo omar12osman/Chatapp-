fb769d31a6114f597cd8ac273f3a8a4fb8259b98
Majid Osman <omar12osman12@gmail.com>
2018-11-25 19:36:19 +0100

    Initial Commit

---
+++ Chatapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chatapp
+//
+//  Created by Majid Osman on 2018-11-25.
+//  Copyright © 2018 Majid Osman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
